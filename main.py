0a8d0d89d3f08ecf7989deb5cce9691c1ae4c453
liya-diya liya <liyadiya749@gmail.com>
2023-03-30 16:38:24 +0000

    navigation pblm resolved

--- main.py
+++ main.py
@@ -4,4 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('base.html')
+    return render_template('index.html')
+@app.route("/instagram")
+def instagram():
+    return render_template('instagram.html')
+@app.route("/facebook")
+def facebook():
+    return render_template('facebook.html')
+@app.route("/twitter")
+def twitter():
+    return render_template('twitter.html')
+@app.route("/fakenews")
+def fakenews():
+    return render_template('fakenews.html')
+@app.route("/about")
+def about():
+    return render_template('about.html')
